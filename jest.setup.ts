ee63cb197408691d6510ec1a91bdc7b90ad20035
Bruno Gonçalves <brunogon.silva@hotmail.com>
2026-01-19 13:59:17 -0300

    update

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,17 @@
-import "@testing-library/jest-dom";
+import "@testing-library/jest-dom"
 
-import fetchMock from "jest-fetch-mock";
+import fetchMock from "jest-fetch-mock"
+import * as React from "react"
+import { act } from "react-dom/test-utils"
 
-// habilita o mock global do fetch
-fetchMock.enableMocks();
+fetchMock.enableMocks()
+
+// adiciona act ao namespace React de forma tipada
+declare module "react" {
+   
+  export function act(
+    callback: () => void | Promise<void>
+  ): Promise<void>
+}
+
+;(React as unknown as { act: typeof act }).act = act
